cc40107154850cfa2c56e186015bb2b19474cd98
okey00789@gmail.com <okey00789@gmail.com@d15928ca-5342-9933-567a-e93c2f04370a>
2010-10-27 01:48:00 +0000

    用于与后台交互的Ajax，分两部分，sinaApp,kkApp

---
+++ js/ajax/app.js
@@ -0,0 +1,19 @@
+/**
+ * @author Administrator
+ */
+var sinaApp={
+	moreComments:function(id){
+		
+	},
+	sendComment:function(id){
+		var content=$('#comment-content-'+id).val();
+		alert("评论:"+content);
+	},
+	sendRetweet:function(id){
+		var content=$('#retweet-content-'+id).val();
+		alert("转发:"+content);
+	}
+}
+var kkApp={
+	
+}
