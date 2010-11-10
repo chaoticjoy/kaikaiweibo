9213cedc02750838e588ea95e5654afcf06304ad
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2010-11-10 14:46:04 +0000

    修正alter提示乱码

--- trunk/login_action.php
+++ trunk/login_action.php
@@ -16,14 +16,14 @@ include_once('inc/sina.php');
 				echo "<script language=\"javascript\">location.href='index.php';</script>";
 			}
 			else{
-				echo "<script language=\"javascript\">alert('登陆失败，请重新登录。');</script>";
+				echo "<script language=\"javascript\">alert('\u767B\u9646\u5931\u8D25\uFF01\u8BF7\u91CD\u65B0\u767B\u9646\u3002');</script>";
 				echo "<script language=\"javascript\">location.href='login.php';</script>";
 			}
 			
 	} 
 	else {
 		//echo "<p>非法请求，请返回</p>";
-		echo "<script language=\"javascript\">alert('非法请求!');</script>";
+		echo "<script charset=\"gb2312\" language=\"javascript\">alert('\u975E\u6CD5\u8BF7\u6C42\uFF01');</script>";
 		echo "<script language=\"javascript\">location.href='index.php';</script>";
 	}
 ?>
