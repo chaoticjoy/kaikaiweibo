ccf7024d783b5f0a517db1da0817b82ef3cb49c0
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2010-11-12 12:42:19 +0000

    followers和friends方法修正cursor参数

--- ajax/followers.php
+++ ajax/followers.php
@@ -2,8 +2,8 @@
 
 include_once('../inc/sina.php');
 	
-	if($_REQUEST['count']&&$_REQUEST['page'])
-		get_followers($_REQUEST['screen_name'],$_REQUEST['count'],$_REQUEST['page']);
+	if($_REQUEST['count']&&$_REQUEST['cursor'])
+		get_followers($_REQUEST['screen_name'],$_REQUEST['count'],$_REQUEST['cursor']);
 	else
 		get_followers();
 ?>
--- ajax/friends.php
+++ ajax/friends.php
@@ -2,8 +2,8 @@
 
 include_once('../inc/sina.php');
 	
-	if($_REQUEST['count']&&$_REQUEST['page'])
-		get_friends($_REQUEST['screen_name'],$_REQUEST['count'],$_REQUEST['page']);
+	if($_REQUEST['count']&&$_REQUEST['cursor'])
+		get_friends($_REQUEST['screen_name'],$_REQUEST['count'],$_REQUEST['cursor']);
 	else
 		get_friends();
 ?>
