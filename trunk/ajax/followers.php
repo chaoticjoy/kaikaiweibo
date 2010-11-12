13473334db2063984f392648da22f3f97973aa5e
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2010-11-12 14:26:50 +0000

    完善初始化followers.php和friends.php

--- trunk/ajax/followers.php
+++ trunk/ajax/followers.php
@@ -4,6 +4,8 @@ include_once('../inc/sina.php');
 	
 	if($_REQUEST['count']&&$_REQUEST['cursor'])
 		get_followers($_REQUEST['screen_name'],$_REQUEST['count'],$_REQUEST['cursor']);
+	elseif($_REQUEST['screen_name'])
+		get_followers($_REQUEST['screen_name']);
 	else
 		get_followers();
 ?>
--- trunk/ajax/friends.php
+++ trunk/ajax/friends.php
@@ -4,6 +4,8 @@ include_once('../inc/sina.php');
 	
 	if($_REQUEST['count']&&$_REQUEST['cursor'])
 		get_friends($_REQUEST['screen_name'],$_REQUEST['count'],$_REQUEST['cursor']);
+	elseif($_REQUEST['screen_name'])
+		get_friends($_REQUEST['screen_name']);
 	else
 		get_friends();
 ?>
