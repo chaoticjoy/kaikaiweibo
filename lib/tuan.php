c4de3123df5b765bfadf21f5b26c478944496c48
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2011-01-16 14:07:45 +0000

    tuangou.php 团购接口

--- lib/tuan.php
+++ lib/tuan.php
@@ -19,6 +19,33 @@
 		//return Array2Json($xml);
 	}
 	
-	print_r(tuan());
-
+	function tuan_city_list(){
+		$content=fetchurl("http://open.client.lashou.com/list/cities/");
+		$xml=simplexml_load_string($content,"SimpleXMLElement",LIBXML_NOCDATA);
+		$result=json_encode($xml,JSON_FORCE_OBJECT);
+		return json_decode( $result, true);
+	}
+	
+	function tuan_city($cityid){
+		$content=fetchurl("http://open.client.lashou.com/list/goods/cityid/".$cityid);
+		$xml=simplexml_load_string($content,"SimpleXMLElement",LIBXML_NOCDATA);
+		$result=json_encode($xml,JSON_FORCE_OBJECT);
+		return json_decode( $result, true);
+	}
+	
+	function tuan_get_cityid($city="全国"){
+		if($city=="全国")
+			return 9999;
+		$cities=tuan_city_list();
+		$cities=$cities['city'];
+		foreach($cities as $key=>$item){
+			if($item['cityname']==$city)
+				return $cities[$key]['cityid'];
+		}
+		return 9999;
+	}
+	
+/*  	$cityid=tuan_get_cityid("北京");
+	print_r(tuan_city($cityid)); 
+ */
 ?>
