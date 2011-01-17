740b4c7e50f2fa1526e615f351ea591de5e6866c
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2011-01-17 12:48:31 +0000

    kk_login_action.php根据情况返回true or false

--- trunk/kk_login_action.php
+++ trunk/kk_login_action.php
@@ -27,17 +27,15 @@ include_once('inc/utility.php');
 			
 			if ($result ) {
 				//echo "<p>登陆成功</p>";
-				echo "<script language=\"javascript\">location.href='index.php';</script>";
+				echo 'True';
 			}
 			else{
-				echo "<script language=\"javascript\">alert('\u767B\u9646\u5931\u8D25\uFF01\u8BF7\u91CD\u65B0\u767B\u9646\u3002');</script>";
-				echo "<script language=\"javascript\">location.href='kk_login.php';</script>";
+				echo 'True';
 			}
 			
 	} 
 	else {
 		//echo "<p>非法请求，请返回</p>";
-		echo "<script charset=\"gb2312\" language=\"javascript\">alert('\u975E\u6CD5\u8BF7\u6C42\uFF01');</script>";
-		echo "<script language=\"javascript\">location.href='kaikai_login.php';</script>";
+		echo 'True';
 	}
 ?>
