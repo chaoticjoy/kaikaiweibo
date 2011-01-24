521bf6c5410c4cdb557a59fbad4e197d54f172a0
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2011-01-24 03:41:20 +0000

    weather本地图标不全，改为用google提供的

--- lib/weather.php
+++ lib/weather.php
@@ -19,8 +19,9 @@
 	function gg_weather($city='北京'){
 		$content=fetchurl("http://www.google.com/ig/api?hl=zh-cn&weather=".urlencode($city));
 		$content=iconv('gb2312','utf-8',$content);
-		$content=str_replace('/ig/images/weather','../image/weather',$content);
-		$content=str_replace('.gif','.png',$content);
+		/* $content=str_replace('/ig/images/weather','../image/weather',$content);
+		$content=str_replace('.gif','.png',$content); */
+		$content=str_replace('/ig/images/weather','http://www.google.ru/ig/images/weather',$content);
 		$xml=simplexml_load_string($content);
 		//urlencode(iconv('gb2312','utf-8',$val['name'])); 
 		$result=json_encode($xml,JSON_FORCE_OBJECT);
