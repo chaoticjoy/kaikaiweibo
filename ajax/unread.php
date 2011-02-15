bb43f849e9b64507263ab16ffac82c6a2a8e09e5
chaoticjoy <chaoticjoy@d15928ca-5342-9933-567a-e93c2f04370a>
2011-02-15 14:17:16 +0000

    添加个人面页：personal_page.php；未读消息：unread.php；清除未读消息：reset_count.php

---
+++ ajax/unread.php
@@ -0,0 +1,23 @@
+﻿<?php
+	include_once('../lib/weibo.php');
+	include_once('../inc/config.php');
+	include_once('../inc/utility.php');
+	
+	$w = new weibo( APP_KEY );
+	$w->setUser( getEncryptCookie('sina_name') , getEncryptCookie('sina_pw') );
+	if($_REQUEST['with_new_status']&&$_REQUEST['since_id'])
+		$unread=$w->unread($_REQUEST['with_new_status'],$_REQUEST['since_id']);
+	else
+		$unread=$w->unread();
+	//print_r($unread);
+	echo "followers:".$unread['followers'];
+	echo "<br>";
+	echo "dm:".$unread['dm'];
+	echo "<br>";
+	echo "mentions:".$unread['mentions'];
+	echo "<br>";
+	echo "comments:".$unread['comments'];
+	echo "<br>";
+	echo "new_status:".$unread['new_status'];
+	echo "<br>";
+?>
